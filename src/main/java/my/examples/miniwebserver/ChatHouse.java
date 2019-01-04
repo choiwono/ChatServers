388283c9b750859f2dd42df015ac5baadd57e1ef
choiwono <treasure_b@naver.com>
2019-01-04 21:28:16 +0900

    #19.01.04 채팅서버

--- src/main/java/my/examples/miniwebserver/ChatHouse.java
+++ src/main/java/my/examples/miniwebserver/ChatHouse.java
@@ -17,41 +17,60 @@ public class ChatHouse {
     public synchronized void createRoom(ChatUser chatUser, String title, boolean flag){
         int maxNum = roomNumber++;
         ChatRoom chatRoom = new ChatRoom(chatUser, title, maxNum, flag);
-        chatUser.setGrade(1);
+        chatUser.setGrade(0);
+        chatUser.setStatus(1);
         chatRooms.add(chatRoom);
+        lobby.remove(chatUser);
+        chatUser.write("방에 입장하셨습니다.");
     }
-    public synchronized List<ChatRoom> getChatRoom(int roomnumber) {
-        List<ChatRoom> list = new ArrayList<ChatRoom>();
+
+    public synchronized ChatRoom getChatRoom(int roomnumber) {
         for(int i=0; i<chatRooms.size(); i++) {
             ChatRoom chatRoom = chatRooms.get(i);
             if(chatRoom.getRoomNumber() == roomnumber) {
-                list.add(chatRoom);
+                return chatRoom;
             }
         }
-        return list;
+        return null;
     }
 
-    public synchronized void setMaster(int roomnumber,String name, int grade) {
-        List<ChatRoom> list = getChatRoom(roomnumber);
-        List<ChatUser> chatUsers = list.get(0).getChatUsers();
+    public synchronized ChatUser getUser(int roomnumber,String name) {
+        ChatRoom chatRoom = getChatRoom(roomnumber);
+        List<ChatUser> chatUsers = chatRoom.getChatUsers();
         for(ChatUser user:chatUsers) {
             if(user.getNickname().equals(name)) {
-                user.setGrade(grade);
-                user.write("방장으로 임명되셨습니다.");
+                return user;
             }
         }
+        return null;
     }
 
-    public synchronized void getOut(String nickname, ChatUser chatUser){
-        List<ChatRoom> list = getChatRoom(chatUser.getRoomNumber());
-        List<ChatUser> chatUsers = list.get(0).getChatUsers();
-        for(int j=0; j<chatUsers.size(); j++) {
-            ChatUser user = chatUsers.get(j);
-            if (user.getNickname().equals(nickname)) {
-                user.write("방장에 의해서 강퇴당하셨습니다.");
-                user.setStatus(0);
-                lobby.add(chatUsers.get(j));
-                chatUsers.remove(j);
+    public synchronized void setMaster(int roomnumber,String name) {
+        ChatUser chatUser = getUser(roomnumber,name);
+        chatUser.setGrade(0);
+        chatUser.write("방장으로 임명되셨습니다.");
+    }
+
+    /**
+     * 방장인 경우 닉네임을 받아서 강퇴시킨다.
+     * @param Strname
+     * @param chatUser
+     **/
+
+    public synchronized void getOut(String Strname, ChatUser chatUser){
+        if(chatUser.getStatus() == 0) { // 방장이 아닌경우
+            chatUser.write("권한이 없습니다.");
+        } else { // 방장인 경우
+            ChatRoom chatRoom = getChatRoom(chatUser.getRoomNumber());
+            List<ChatUser> chatUsers = chatRoom.getChatUsers();
+            for (int j = 0; j < chatUsers.size(); j++) {
+                ChatUser user = chatUsers.get(j);
+                if (user.getNickname().equals(Strname)) {
+                    user.write("방장에 의해서 강퇴당하셨습니다.");
+                    user.setStatus(0);
+                    lobby.add(chatUsers.get(j));
+                    chatUsers.remove(j);
+                }
             }
         }
     }
@@ -71,7 +90,7 @@ public class ChatHouse {
         lobby.remove(chatUser);
     }
 
-    public synchronized List<ChatUser> getUser(ChatUser chatUser) {
+    public synchronized List<ChatUser> getUsers(ChatUser chatUser) {
         for(ChatRoom cr : chatRooms){
             if(cr.existsUser(chatUser)){
                 return cr.getChatUsers();
@@ -84,24 +103,48 @@ public class ChatHouse {
         return chatRooms;
     }
 
-    public synchronized boolean joinRoom(int roomNum, ChatUser chatUser) {
-        ChatRoom chatRoom = chatRooms.get(roomNum);
+    public synchronized void joinMessage(int roomNumber,ChatUser chatUser) {
+        ChatRoom chatRoom = chatRooms.get(roomNumber);
+        chatUser.setStatus(1);
+        chatUser.setGrade(1);
+        chatRoom.addChatUser(chatUser);
+        chatUser.setRoomNumber(roomNumber);
+        lobby.remove(chatUser);
+        chatUser.write(roomNumber+"번방에 입장하셨습니다.");
+        List<ChatUser> chatUsers = chatRoom.getChatUsers();
+        for(ChatUser list:chatUsers) {
+            if(!list.getNickname().equals(chatUser.getNickname())) {
+                list.write(chatUser.getNickname() + "님이 입장하셨습니다.");
+            }
+        }
+    }
+
+    public synchronized void joinRoom(int roomNumber, ChatUser chatUser) {
+        ChatRoom chatRoom = chatRooms.get(roomNumber);
         if(chatRoom.isFlag() == true) {
-            chatRoom.addChatUser(chatUser);
-            List<ChatUser> chatUsers = chatRoom.getChatUsers();
-            for(ChatUser list:chatUsers) {
-                if(!list.getNickname().equals(chatUser.getNickname())) {
-                    list.write(chatUser.getNickname() + "님이 입장하셨습니다.");
+            joinMessage(roomNumber,chatUser);
+        } else if(chatRoom.isFlag() == false){
+            chatUser.write("비밀방입니다.");
+            chatUser.write("입장하실려면 비밀번호를 입력해주세요.");
+            while(true) {
+                String password = chatUser.read();
+                if (chatRoom.getPassword().equals(password)) {
+                    joinMessage(roomNumber, chatUser);
+                    break;
+                } else {
+                    chatUser.write("비밀번호가 틀렸습니다.");
+                    break;
                 }
             }
         }
-        return chatRoom.isFlag();
     }
 
-    public synchronized void outRoom(ChatUser chatUser) {
-        List<ChatRoom> list = getChatRoom(chatUser.getRoomNumber());
-        list.get(0).remove(chatUser);
-        List<ChatUser> chatUsers = list.get(0).getChatUsers();
+    public synchronized void outRoom(ChatUser chatUser) { // 방나가기 기능
+        ChatRoom chatRoom = getChatRoom(chatUser.getRoomNumber());
+        chatUser.setStatus(0);
+        chatUser.setGrade(1);
+        chatRoom.remove(chatUser);
+        List<ChatUser> chatUsers = chatRoom.getChatUsers();
         for(ChatUser member : chatUsers) {
             if(!member.equals(chatUser.getNickname())) {
                 member.write(chatUser.getNickname()+"님이 나가셨습니다.");
@@ -110,21 +153,47 @@ public class ChatHouse {
     }
 
     public synchronized void secretRoom(ChatUser chatUser, String password) {
-        List<ChatRoom> list = getChatRoom(chatUser.getRoomNumber());
-        ChatRoom chatRoom = list.get(0);
+        ChatRoom chatRoom = getChatRoom(chatUser.getRoomNumber());
         chatRoom.setPassword(password);
         chatRoom.setFlag(false);
+        chatUser.write(password);
+        chatUser.write(String.valueOf(chatRoom.isFlag()));
     }
 
     public synchronized void invite(String inviteName, int roomNumber){
-        List<ChatRoom> list = getChatRoom(roomNumber);
-        ChatRoom chatRoom = list.get(0);
+        ChatRoom chatRoom = getChatRoom(roomNumber);
         for(int i=0; i<lobby.size(); i++) {
             ChatUser member = lobby.get(i);
             if(member.getNickname().equals(inviteName)) {
                 member.setStatus(1);
                 chatRoom.getChatUsers().add(member);
                 lobby.remove(i);
+            }
+        }
+    }
+
+    public void printHelp(ChatUser chatUser) {
+        if(chatUser.getStatus() == 0) {
+            chatUser.write("1. 채팅방 생성 : /create 방제목");
+            chatUser.write("2. 목록 확인 : /list");
+            chatUser.write("3. 채팅방 참여 : /join 방번호");
+        } else {
+            chatUser.write("1. 채팅방 인원확인 : /member");
+            chatUser.write("2. 채팅방 나가기 : /out");
+            chatUser.write("3. 채팅방 비밀번호 지정 : /password");
+            chatUser.write("4. 방장 넘기기 : /master 닉네임");
+            chatUser.write("5. 강퇴 시키기 : /getout 닉네임");
+            chatUser.write("6. 로비 인원초대 : /invite 닉네임");
+        }
+    }
+
+    public void getMembers(List<ChatUser> chatUsers, ChatUser chatUser) {
+        for (int i = 0; i < chatUsers.size(); i++) {
+            ChatUser user = chatUsers.get(i);
+            if(user.getGrade() == 0) {
+                chatUser.write("[방장]"+user.getNickname());
+            } else if(user.getGrade() == 1){
+                chatUser.write("[일반]"+user.getNickname());
             }
         }
     }
--- src/main/java/my/examples/miniwebserver/ChatServerHandler.java
+++ src/main/java/my/examples/miniwebserver/ChatServerHandler.java
@@ -35,83 +35,52 @@ public class ChatServerHandler extends Thread{
                         String title = message.substring(message.indexOf(" ") + 1);
                         boolean flag = true;
                         chatHouse.createRoom(chatUser, title, flag);
-                        chatUser.write("방에 입장하셨습니다.");
-                        chatUser.setStatus(1);
-                        //inRoom = true;
+
                     } else if(message.indexOf("/list") == 0) {
                         for(ChatRoom cr : chatRooms){
                             chatUser.write(cr.getRoomNumber()+ " : " + cr.getTitle());
                         }
-                    } else if(message.indexOf("/quit") == 0){
-                        inRoom = false;
+
                     } else if(message.indexOf("/join") == 0){
                         String strRoomNum = message.substring(message.indexOf(" ") +1);
-
                         int roomNum = Integer.parseInt(strRoomNum);
-                        boolean result = chatHouse.joinRoom(roomNum, chatUser);
-
-                        //String ms = String.valueOf(result);
-                        //chatUser.write(ms);
-                        if(result == true) {
-                            chatUser.setRoomNumber(roomNum);
-                            chatUser.write(roomNum+"번방에 입장하셨습니다.");
-                            chatHouse.exit(chatUser);
-                            chatUser.setStatus(1);
-                        } else {
-                            chatUser.write("비밀방입니다.");
-                            chatUser.setStatus(0);
-                        }
+                        chatHouse.joinRoom(roomNum, chatUser);
 
                     } else if(message.indexOf("/help") == 0) {
-                        chatUser.write("1. 채팅방 생성 : /create 방제목");
-                        chatUser.write("2. 목록 확인 : /list");
-                        chatUser.write("3. 채팅방 참여 : /join 방번호");
-                        chatUser.setStatus(0);
+                        chatHouse.printHelp(chatUser);
                     }
+
                 } else if(chatUser.getStatus() == 1) { // 방안에 있을 경우
-                    List<ChatUser> chatUsers = chatHouse.getUser(chatUser);
-                    List<ChatRoom> chatRooms = chatHouse.getChatRooms();
+                    List<ChatUser> chatUsers = chatHouse.getUsers(chatUser);
                     if (message.indexOf("/member") == 0) {
-                        for (int i = 0; i < chatUsers.size(); i++) {
-                            String grade;
-                            ChatUser user = chatUsers.get(i);
-                            if(user.getGrade() == 1){
-                                grade = "방장";
-                            }else{
-                                grade = "일반";
-                            }
-                            chatUser.write("["+grade+"]"+chatUsers.get(i).getNickname());
-                        }
+                        chatHouse.getMembers(chatUsers, chatUser);
+
+                    } else if(message.indexOf("/help") == 0) {
+                        chatHouse.printHelp(chatUser);
+
                     } else if (message.indexOf("/out") == 0) {
                         chatUser.write("방에서 나가셨습니다.");
-                        chatHouse.addChatUser(chatUser);
                         chatHouse.outRoom(chatUser);
-                        chatUser.setStatus(0);
+                        chatHouse.addChatUser(chatUser);
 
-                    } else if (message.indexOf("/password") == 0) {
+                    } else if (message.indexOf("/secret") == 0) {
                         String password = message.substring(message.indexOf(" ") + 1);
                         chatHouse.secretRoom(chatUser, password);
                         chatUser.write("비밀방이 설정되셨습니다.");
 
                     } else if (message.indexOf("/master") == 0) {
                         String name = message.substring(message.indexOf(" ") + 1);
-                        int roomNum = chatUser.getRoomNumber();
-                        int grade = 1;
-                        chatHouse.setMaster(roomNum, name, grade);
-                        chatUser.setGrade(0);
-
+                        chatHouse.setMaster(chatUser.getRoomNumber(), name);
+                        chatUser.setGrade(1);
                     } else if(message.indexOf("/getout") == 0) {
-                        if (chatUser.getGrade() == 1) {
-                            String memName = message.substring(message.indexOf(" ") + 1);
-                            chatHouse.getOut(memName, chatUser);
-                            chatUser.write("["+memName+"]"+"님을 강퇴 하셨습니다.");
-                        } else {
-                            chatUser.write("권한이 없습니다.");
-                        }
+                        String strName = message.substring(message.indexOf(" ") + 1);
+                        chatHouse.getOut(strName, chatUser);
+
                     } else if (message.indexOf("/invite")==0){
                         String inviteName = message.substring(message.indexOf(" ")+1);
                         chatHouse.invite(inviteName, chatUser.getRoomNumber());
                         chatUser.write(inviteName+"님을 초대하셨습니다.");
+
                     } else {
                         for(ChatUser cu : chatUsers){
                           cu.write(chatUser.getNickname() + " : " + message);
